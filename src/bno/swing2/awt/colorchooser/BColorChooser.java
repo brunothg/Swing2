ad3f91ab13e0a9759eb3ade96d42109297f560b1
Marvin Bruns <Marvin Bruns@n-marvin>
2014-05-27 14:56:10 +0200

    BColorChooser

---
+++ src/bno/swing2/awt/colorchooser/BColorChooser.java
@@ -0,0 +1,48 @@
+package bno.swing2.awt.colorchooser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JColorChooser;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
+import javax.swing.colorchooser.ColorSelectionModel;
+
+import bno.swing2.awt.BColor;
+
+public class BColorChooser extends JColorChooser {
+
+	private static final long serialVersionUID = 2889510211457614799L;
+
+	public BColorChooser() {
+		super();
+		addHex();
+	}
+
+	public BColorChooser(ColorSelectionModel model) {
+		super(model);
+		addHex();
+	}
+
+	public BColorChooser(BColor initialColor) {
+		super(initialColor);
+		addHex();
+	}
+
+	private void addHex() {
+
+		AbstractColorChooserPanel[] panelsAr = getChooserPanels();
+
+		List<AbstractColorChooserPanel> panels = new ArrayList<>(
+				panelsAr.length + 1);
+
+		for (AbstractColorChooserPanel pl : panelsAr) {
+			panels.add(pl);
+		}
+
+		panels.add(0, new BHexColorChooserPanel());
+
+		setChooserPanels(panels.toArray(new AbstractColorChooserPanel[panels
+				.size()]));
+	}
+
+}
--- src/bno/swing2/test/TestJPanel.java
+++ src/bno/swing2/test/TestJPanel.java
@@ -1,119 +1,35 @@
 package bno.swing2.test;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import bno.swing2.awt.BColor;
 import bno.swing2.awt.BGraphics2D;
-import bno.swing2.awt.ColorChangeEvent;
-import bno.swing2.awt.ColorChangeListener;
-import bno.swing2.awt.colorchooser.gradient.BGradientColorChooserWidget;
-import bno.swing2.awt.colorchooser.hexcolorchooser.BHexColorChooserWidget;
+import bno.swing2.awt.colorchooser.BColorChooser;
 
 public class TestJPanel extends JPanel implements KeyListener {
 
 	private static final long serialVersionUID = 6360279416330156160L;
-	private BGradientColorChooserWidget gradient2;
 
 	public TestJPanel() {
 		super();
 		setFocusable(true);
 		addKeyListener(this);
 		setLayout(new BorderLayout());
+		setOpaque(true);
 
-		JPanel top = new JPanel();
-		top.setLayout(new FlowLayout());
-		add(top, BorderLayout.NORTH);
+		BColorChooser cc = new BColorChooser();
+		add(cc, BorderLayout.CENTER);
 
-		comp = new BHexColorChooserWidget();
-
-		final JLabel hexColorName = new JLabel(comp.getSelectedColor()
-				.toHexString());
-		top.add(hexColorName);
-
-		final JLabel colorName = new JLabel(comp.getSelectedColor()
-				.toRGBString());
-		top.add(colorName);
-
-		final JLabel color = new JLabel("Selected Color");
-		color.setOpaque(true);
-		color.setBackground(comp.getSelectedColor());
-		color.setForeground(comp.getSelectedColor().invertRGB());
-		top.add(color);
-
-		comp.addColorChangeListener(new ColorChangeListener() {
-
-			@Override
-			public void selectedColorChanged(ColorChangeEvent ev) {
-				System.out.printf("Hex Color changed %s -> %s%n", ev
-						.getOldColor().toString(), ev.getNewColor().toString());
-				hexColorName.setText(ev.getNewColor().toHexString());
-				colorName.setText(ev.getNewColor().toRGBString());
-				color.setBackground(ev.getNewColor());
-				color.setForeground(ev.getNewColor().invertRGB());
-				gradient.setColor(ev.getNewColor());
-				gradient2.setColor(ev.getNewColor());
-			}
-
-			@Override
-			public void mouseOverColorChanged(ColorChangeEvent ev) {
-				System.out.printf("Hex Mouse over Color changed %s -> %s%n",
-						(ev.getOldColor() != null) ? ev.getOldColor()
-								.toString() : "NULL",
-						(ev.getNewColor() != null) ? ev.getNewColor()
-								.toString() : "NULL");
-			}
-		});
-
-		add(comp, BorderLayout.CENTER);
-
-		gradient = new BGradientColorChooserWidget(new BColor(
-				comp.getSelectedColor()), BGradientColorChooserWidget.Y_AXIS);
-		add(gradient, BorderLayout.EAST);
-
-		gradient2 = new BGradientColorChooserWidget(new BColor(
-				comp.getSelectedColor()), BGradientColorChooserWidget.X_AXIS);
-		gradient2.setMaximumSubdivisions(40);
-		add(gradient2, BorderLayout.SOUTH);
-
-		ColorChangeListener gccl = new ColorChangeListener() {
-
-			@Override
-			public void selectedColorChanged(ColorChangeEvent ev) {
-				System.out.printf("Grad Color changed %s -> %s%n", ev
-						.getOldColor().toString(), ev.getNewColor().toString());
-				hexColorName.setText(ev.getNewColor().toHexString());
-				colorName.setText(ev.getNewColor().toRGBString());
-				color.setBackground(ev.getNewColor());
-				color.setForeground(ev.getNewColor().invertRGB());
-
-				gradient2.setColor(ev.getNewColor());
-				gradient.setColor(ev.getNewColor());
-			}
-
-			@Override
-			public void mouseOverColorChanged(ColorChangeEvent ev) {
-				System.out.printf("Grad Mouse over Color changed %s -> %s%n",
-						(ev.getOldColor() != null) ? ev.getOldColor()
-								.toString() : "NULL",
-						(ev.getNewColor() != null) ? ev.getNewColor()
-								.toString() : "NULL");
-			}
-		};
-
-		gradient.addColorChangeListener(gccl);
-		gradient2.addColorChangeListener(gccl);
+		cc.setColor(BColorChooser
+				.showDialog(this, "Farbe aussuchen", Color.RED));
 	}
 
 	int zoom = 300;
-	private BHexColorChooserWidget comp;
-	private BGradientColorChooserWidget gradient;
 
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -144,8 +60,6 @@ public class TestJPanel extends JPanel implements KeyListener {
 			break;
 		}
 
-		comp.setSelectedColor(new BColor((int) (Math.random() * 255),
-				(int) (Math.random() * 255), (int) (Math.random() * 255)));
 	}
 
 	@Override
